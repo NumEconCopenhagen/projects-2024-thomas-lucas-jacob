6ef73ec9eb529d33958e1b5c855c75dc99ffcdc4
zdj807 <zdj807@alumni.ku.dk>
2024-04-14 12:11:23 +0200

    merg con

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -1,20 +1,3 @@
-def keep_regs(df, regs):
-    """ Example function. Keep only the subset regs of regions in data.
-
-    Args:
-        df (pd.DataFrame): pandas dataframe 
-
-    Returns:
-        df (pd.DataFrame): pandas dataframe
-
-    """ 
-    
-    for r in regs:
-        I = df.reg.str.contains(r)
-        df = df.loc[I == False] # keep everything else
-    
-    return df
-
 # installing API reader, that will allow to load data from DST.
 %pip install git+https://github.com/alemartinello/dstapi
 %pip install pandas-datareader
@@ -22,14 +5,23 @@ def keep_regs(df, regs):
 import pandas_datareader # install with `pip install pandas-datareader`
 from dstapi import DstApi # install with `pip install git+https://github.com/alemartinello/dstapi`
 
-supply_balance = DstApi('NAN1') #import NAN1 from DST
-params = supply_balance._define_base_params(language= 'en') #defines all parameters in NAN1
-SB1 = supply_balance.get_data(params=params) #creates a dataframe from the supply balance
-SB2=SB1[SB1['PRISENHED']=='2010-prices, chained values, (bill. DKK.)'] #create new DF, filtered for price type = chained prices (2010)
-supply_balance_import=SB2[SB2['TRANSAKT'] == 'B.1*g Gross domestic product']  #create new DF, filtered only BNP
-del supply_balance_import['TRANSAKT'] #deletes the TRANSAKT column
-del supply_balance_import['PRISENHED'] #deletes the PRISENHED column
+def supply_balance_DK:
+    supply_balance = DstApi('NAN1') #import NAN1 from DST
+    params = supply_balance._define_base_params(language= 'en') #defines all parameters in NAN1
+    SB1 = supply_balance.get_data(params=params) #creates a dataframe from the supply balance
+    SB1_1= SB1[SB1['TID'] >= 1990] #removes data from before 1990
+    SB2=SB1[SB1['PRISENHED']=='2010-prices, chained values, (bill. DKK.)'] #create new DF, filtered for price type = chained prices (2010)
+    supply_balance_BNP=SB2[SB2['TRANSAKT'] == 'B.1*g Gross domestic product']  #create new DF, filtered only BNP
+    del supply_balance_BNP['TRANSAKT'] #deletes the TRANSAKT column
+    del supply_balance_BNP['PRISENHED'] #deletes the PRISENHED column
+    return supply_balance_BNP
 
-Q_bnp = DstApi('NKN1')
-
-
+def quartly_BNP:
+    Q_bnp = DstApi('NKN1')
+    params_q = Q_bnp._define_base_params(language= 'en') #defines all parameters in NAN1
+    Q1 = Q_bnp.get_data(params=params_q) #creates a dataframe from the supply balance
+    Q2=Q1[Q1['PRISENHED']=='2010-prices, chained values, (bill. DKK.)'] #create new DF, filtered for price type = chained prices (2010)
+    BNP_quarterly=Q2[Q2['TRANSAKT'] == 'B.1*g Gross domestic product']  #create new DF, filtered only BNP
+    del BNP_quarterly['TRANSAKT'] #deletes the TRANSAKT column
+    del BNP_quarterly['PRISENHED'] #deletes the PRISENHED column
+    return BNP_quarterly
