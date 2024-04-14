acb8e7be15ab7f961156f2519273c9b96b9354df
zdj807 <zdj807@alumni.ku.dk>
2024-04-14 14:09:26 +0200

    fgh

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -65,14 +65,14 @@ def quarterly_BNP():
     return BNP_quarterly_sorted
 
 def quarterly_BNP_for_mov_avg():
-    Q_bnp = DstApi('NKN1')
-    params_q = Q_bnp._define_base_params(language= 'en') #defines all parameters in NAN1
-    Q1 = Q_bnp.get_data(params= params_q) #creates a dataframe from the supply balance
-    Q2=Q1[Q1['PRISENHED']=='2010-prices, chained values, (bill. DKK.)'] #create new DF, filtered for price type = chained prices (2010)
-    Q3=Q2[Q2['SÆSON'] == 'Seasonally adjusted']
-    BNP_quarterly=Q3[Q3['TRANSAKT'] == 'B.1*g Gross domestic product']  #create new DF, filtered only BNP
-    del BNP_quarterly['TRANSAKT'] #deletes the TRANSAKT column
-    del BNP_quarterly['PRISENHED'] #deletes the PRISENHED column
-    del BNP_quarterly['SÆSON'] #deletes the SÆSON column
-    BNP_quarterly_sorted = BNP_quarterly.sort_values(by= 'TID') #we sort the dataframe by year
-    return BNP_quarterly_sorted
+    Q_bnp_1 = DstApi('NKN1')
+    params_q_1 = Q_bnp_1._define_base_params(language= 'en') #defines all parameters in NAN1
+    Q1_1 = Q_bnp_1.get_data(params= params_q_1) #creates a dataframe from the supply balance
+    Q2_1=Q1_1[Q1_1['PRISENHED']=='2010-prices, chained values, (bill. DKK.)'] #create new DF, filtered for price type = chained prices (2010)
+    Q3_1=Q2_1[Q2_1['SÆSON'] == 'Seasonally adjusted']
+    BNP_quarterly_1=Q3_1[Q3_1['TRANSAKT'] == 'B.1*g Gross domestic product']  #create new DF, filtered only BNP
+    del BNP_quarterly_1['TRANSAKT'] #deletes the TRANSAKT column
+    del BNP_quarterly_1['PRISENHED'] #deletes the PRISENHED column
+    del BNP_quarterly_1['SÆSON'] #deletes the SÆSON column
+    BNP_quarterly_sorted_1 = BNP_quarterly_1.sort_values(by= 'TID') #we sort the dataframe by year
+    return BNP_quarterly_sorted_1
