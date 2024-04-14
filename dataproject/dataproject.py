df31d1e01a1adfe46b89b284136cf8245edda01d
zdj807 <zdj807@alumni.ku.dk>
2024-04-14 13:03:39 +0200

    sorting

--- dataproject/dataproject.py
+++ dataproject/dataproject.py
@@ -11,7 +11,7 @@ def supply_balance_BNP():
     supply_balance_BNP=SB2[SB2['TRANSAKT'] == 'B.1*g Gross domestic product']  #create new DF, filtered only BNP
     del supply_balance_BNP['TRANSAKT'] #deletes the TRANSAKT column
     del supply_balance_BNP['PRISENHED'] #deletes the PRISENHED column
-    supply_balance_BNP_sorted = supply_balance_BNP.sort_values(by= 'TID')
+    supply_balance_BNP_sorted = supply_balance_BNP.sort_values(by= 'TID') #we sort the dataframe by year
     return supply_balance_BNP_sorted
 
 def supply_balance_import():
@@ -23,7 +23,8 @@ def supply_balance_import():
     supply_balance_import=SB2[SB2['TRANSAKT'] == 'P.7 Imports of goods and services']  #create new DF, filtered only import
     del supply_balance_import['TRANSAKT'] #deletes the TRANSAKT column
     del supply_balance_import['PRISENHED'] #deletes the PRISENHED column
-    return supply_balance_import
+    supply_balance_import_sorted = supply_balance_import.sort_values(by= 'TID') #we sort the dataframe by year
+    return supply_balance_import_sorted
 
 def supply_balance_export():
     supply_balance = DstApi('NAN1') #import NAN1 from DST
@@ -34,7 +35,8 @@ def supply_balance_export():
     supply_balance_export=SB2[SB2['TRANSAKT'] == 'P.6 Exports of goods and services']  #create new DF, filtered only export
     del supply_balance_export['TRANSAKT'] #deletes the TRANSAKT column
     del supply_balance_export['PRISENHED'] #deletes the PRISENHED column
-    return supply_balance_export
+    supply_balance_export_sorted = supply_balance_export.sort_values(by= 'TID') #we sort the dataframe by year
+    return supply_balance_export_sorted
 
 def supply_balance_privat():
     supply_balance = DstApi('NAN1') #import NAN1 from DST
@@ -45,7 +47,8 @@ def supply_balance_privat():
     supply_balance_privat=SB2[SB2['TRANSAKT'] == 'P.31 Private consumption']  #create new DF, filtered only privat consumption
     del supply_balance_privat['TRANSAKT'] #deletes the TRANSAKT column
     del supply_balance_privat['PRISENHED'] #deletes the PRISENHED column
-    return supply_balance_privat
+    supply_balance_privat_sorted = supply_balance_privat.sort_values(by= 'TID') #we sort the dataframe by year
+    return supply_balance_privat_sorted
 
 def quarterly_BNP():
     Q_bnp = DstApi('NKN1')
@@ -57,4 +60,5 @@ def quarterly_BNP():
     del BNP_quarterly['TRANSAKT'] #deletes the TRANSAKT column
     del BNP_quarterly['PRISENHED'] #deletes the PRISENHED column
     del BNP_quarterly['SÆSON'] #deletes the SÆSON column
+    BNP_quarterly_sorted = BNP_quarterly.sort_values(by= 'TID') #we sort the dataframe by year
     return BNP_quarterly
