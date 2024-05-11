e5b1410b0de68fe32238decc963a48917336718d
LucasJantunen <lucas.jantunen@gmail.com>
2024-05-11 12:17:34 +0200

    Maltus

--- modelproject/MalthusModel.py
+++ modelproject/MalthusModel.py
@@ -1,1 +1,46 @@
 
+
+##### MALTUS MODEL
+
+from scipy import optimize
+from types import SimpleNamespace
+import sympy as sm
+import numpy as np
+
+
+class Malthus:
+
+    def __init__(self):
+
+        par = self.par = SimpleNamespace()
+        par.alpha = 0.5
+        par.r = 0.05
+        par.LO = 1
+    
+    def Y(self, X, A, L):
+        par = self.par
+
+        return (A*X)**(1-par.alpha)*L**par.alpha
+
+    def y(self, X, A, L):
+        par = self.par
+        
+        return (A*X/L)**par.alpha
+
+    def L(self, t):
+        par = self.par
+        t = np.arrange(0, 1000, 1)
+        return par.LO*exp(par.r*t)*y 
+        
+
+
+
+
+
+
+
+
+
+
+
+
