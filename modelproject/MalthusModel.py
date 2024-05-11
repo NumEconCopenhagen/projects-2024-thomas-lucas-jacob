6c4d118d21a68872053aaa43d8b8703805e37f71
zdj807 <zdj807@alumni.ku.dk>
2024-05-11 13:37:11 +0200

    model

--- modelproject/MalthusModel.py
+++ modelproject/MalthusModel.py
@@ -11,28 +11,61 @@ class Malthus():
     def __init__(self):
 
         par = self.par = SimpleNamespace()
-        par.alpha = 0.5
-        par.r = 0.05 
-        par.LO = 1 
-        par.D = 1
-        par.p = 0.1 
+        par.anul = 0.5
+        par.a = 0.05 
+        par.bnul = 1 
+        par.b = 1
+        par.dnul = 0.1 
+        par.d = 0.1
     
-    def Y_t(self, X, A, L):
+    def Yt(self, Lt):
         par = self.par
 
-        return (A*X)**(1-par.alpha)*L**par.alpha
+        return par.anul-par.a* Lt
 
-    def y_t(self, Y, L):
+    def Bt(self, Yt, Lt):
         par = self.par
+
+        return (par.bnul + par.b* Yt)* Lt
+    
+    def Dt(self, Yt, Lt):
+        par = self.par
+
+        return (par.dnul + par.d1* Yt)* Lt
+
+    def Lt1(self, Yt, Bt, Dt, Lt):
+        par = self.par
+        Yt = Yt(self, Lt)
+        Bt= Bt(self, Yt, Lt)
+        Dt= Dt(self, Yt, Lt)
+
+        return Lt + Bt + Dt
+
+
+
+
+
+
+
+
+
+
+    #def Y_t(self, X, A, L):
+     #   par = self.par
+
+      #  return (A*X)**(1-par.alpha)*L**par.alpha
+
+    #def y_t(self, Y, L):
+     #   par = self.par
         
-        return Y / L
+      #  return Y / L
     
-    def L_t(self_)
+    #def L_t(self_)
 
-    def Lt1(self, t):
-        par = self.par
-        t = np.arrange(0, 1000, 1)
-        return L_t*y_t*par.p - par.D*L_t
+    #def Lt1(self, t):
+     #   par = self.par
+      #  t = np.arrange(0, 1000, 1)
+       # return L_t*y_t*par.p - par.D*L_t
         
     
 
