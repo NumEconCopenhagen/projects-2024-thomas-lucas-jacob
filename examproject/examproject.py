3bf4159371d9a18a275d92c6e333cf4c85efff1d
zdj807 <zdj807@alumni.ku.dk>
2024-06-22 16:11:25 +0200

    dfgv

--- examproject/examproject.py
+++ examproject/examproject.py
@@ -1,5 +1,6 @@
 from types import SimpleNamespace
 import numpy as np
+from scipy.optimize import minimize_scalar
 
 class ProblemOne:
     def __init__(self):
@@ -54,3 +55,50 @@ class ProblemOne:
         y2 = self.output(l2)
         pr2 = self.profit(p2, y2, w, l2)
         return (1-par.alpha)*(w*l + par.T + pr1 + pr2)/(p2+par.tau)
+    
+    def labor_supply_opt(self, w, p1, p2):
+        def neg_utility(l):
+            par = self.par
+            l1_star = self.labor_opt(p1, w)
+            y1_star = self.output_opt(p1, w)
+            pi1_star = self.profit(p1, y1_star, w, l1_star)
+            l2_star = self.labor_opt(p2, w)
+            y2_star = self.output_opt(p2, w)
+            pi2_star = self.profit(p2, y2_star, w, l2_star)
+            c1 = self.consumption1(w, l, pi1_star, pi2_star, p1)
+            c2 = self.consumption2(w, l, pi1_star, pi2_star, p2)
+            return -self.utility(c1, c2, l)
+        
+        result = minimize_scalar(neg_utility, bounds=(0, 1e6), method='bounded')
+        return result.x
+    
+    def labor_clear_con(self, p2, p1, w):
+        par = self.par
+        l_star = self.labor_supply_opt(w, p1, p2)
+        l1_star = self.labor_opt(p1, w)
+        l2_star = self.labor_opt(p2, w)
+        return l_star -(l1_star + l2_star)
+
+    def good1_clear_con(self, p2, p1, w):
+        par = self.par
+        l_star = self.labor_supply_opt(w, p1, p2)
+        l1_star = self.labor_opt(p1, w)
+        y1_star = self.output_opt(p1, w)
+        pi1_star = self.profit(p1, y1_star, w, l1_star)
+        l2_star = self.labor_opt(p2, w)
+        y2_star = self.output_opt(p2, w)
+        pi2_star = self.profit(p2, y2_star, w, l2_star)
+        c1 = self.consumption1(w, l_star, pi1_star, pi2_star, p1)
+        return y1_star - c1
+
+    def good2_clear_con(self, p2, p1, w):
+        par = self.par
+        l_star = self.labor_supply_opt(w, p1, p2)
+        l1_star = self.labor_opt(p1, w)
+        y1_star = self.output_opt(p1, w)
+        pi1_star = self.profit(p1, y1_star, w, l1_star)
+        l2_star = self.labor_opt(p2, w)
+        y2_star = self.output_opt(p2, w)
+        pi2_star = self.profit(p2, y2_star, w, l2_star)
+        c2 = self.consumption1(w, l_star, pi1_star, pi2_star, p2)
+        return y2_star - c2
