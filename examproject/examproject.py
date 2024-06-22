00e19edeed70ca11b76295ad573498020b49acc4
zdj807 <zdj807@alumni.ku.dk>
2024-06-22 18:18:18 +0200

    comments

--- examproject/examproject.py
+++ examproject/examproject.py
@@ -5,6 +5,9 @@ from scipy.optimize import minimize_scalar
 class ProblemOne:
     def __init__(self):
         par = self.par = SimpleNamespace()
+
+        ##parameters set up from problem tekst
+
         par.A = 1.0  # Technology parameter
         par.gamma = 0.5  # Output elasticity of labor
         par.alpha = 0.5  # Preference parameter for good 1
@@ -14,29 +17,29 @@ class ProblemOne:
         par.T = 0.0  # Lump-sum transfer
         par.kappa = 0.1  # Social cost of carbon parameter
 
-    def profit(self, p, y, w, l):
+    def profit(self, p, y, w, l): #definition of the firms profit function
         return p*y - w*l
     
-    def output(self, l):
+    def output(self, l): #firms output functions
         par = self.par
         return par.A * (l**par.gamma)
     
-    def labor_opt(self, p, w):
+    def labor_opt(self, p, w): #firms optimal labor demand
         par = self.par
         x = 1/(1-par.gamma)
         z = (p*par.A*par.gamma)/w
         return z**x
     
-    def output_opt(self, p, w):
+    def output_opt(self, p, w): #firms optimal output, given the optimal labor
         par = self.par
         l = self.labor_opt(p, w)
         return par.A*(l**par.gamma)
     
-    def utility(self, c1, c2, l):
+    def utility(self, c1, c2, l): #Consumers utility function
         par = self.par
         return np.log((c1**par.alpha) * (c2**(1-par.alpha))) - par.nu * ((l**(1+par.epsilon)) / (1+par.epsilon))
     
-    def consumption1(self, w, l, l1, l2, p1, p2):
+    def consumption1(self, w, l, l1, l2, p1, p2): #demand for good 1 / consumption of good 1
         par = self.par
         l1 = self.labor_opt(p1, w)
         y1 = self.output(l1)
@@ -46,7 +49,7 @@ class ProblemOne:
         pr2 = self.profit(p2, y2, w, l2)
         return par.alpha*(w*l + par.T + pr1 + pr2)/p1
 
-    def consumption2(self, w, l, l1, l2, p1, p2):
+    def consumption2(self, w, l, l1, l2, p1, p2): #demand for good 2 / consumption of good 2
         par = self.par
         l1 = self.labor_opt(p1, w)
         y1 = self.output(l1)
@@ -56,8 +59,8 @@ class ProblemOne:
         pr2 = self.profit(p2, y2, w, l2)
         return (1-par.alpha)*(w*l + par.T + pr1 + pr2)/(p2+par.tau)
     
-    def labor_supply_opt(self, w, p1, p2):
-        def neg_utility(l):
+    def labor_supply_opt(self, w, p1, p2): #maximizing the labor supply
+        def neg_utility(l): #negative utility function, for maximization of utility
             par = self.par
             l1_star = self.labor_opt(p1, w)
             y1_star = self.output_opt(p1, w)
@@ -69,24 +72,24 @@ class ProblemOne:
             c2 = self.consumption2(w, l, pi1_star, pi2_star, p1, p2)
             return -self.utility(c1, c2, l)
         
-        result = minimize_scalar(neg_utility, bounds=(0, 1e6), method='bounded')
-        return result.x
+        result = minimize_scalar(neg_utility, bounds=(0, 1e6), method='bounded') #maximizing utility function by minimizing the negative utility function
+        return result.x #returns the labor l which maximises utility
     
-    def labor_clear_con(self, p2, p1, w):
+    def labor_clear_con(self, p2, p1, w): #setting up the clearing condition for the labor market (ready for fsolve of p2)
         par = self.par
         l_star = self.labor_supply_opt(w, p1, p2)
         l1_star = self.labor_opt(p1, w)
         l2_star = self.labor_opt(p2, w)
         return l_star -(l1_star + l2_star)
 
-    def labor_clear_con2(self, p1, p2, w):
+    def labor_clear_con2(self, p1, p2, w): #setting up the clearing condition for the labor market (ready for fsolve of p1)
         par = self.par
         l_star = self.labor_supply_opt(w, p1, p2)
         l1_star = self.labor_opt(p1, w)
         l2_star = self.labor_opt(p2, w)
         return l_star -(l1_star + l2_star)
 
-    def good1_clear_con(self, p2, p1, w):
+    def good1_clear_con(self, p2, p1, w): #setting up the clearing condition for the good 1 market (ready for fsolve of p2)
         par = self.par
         l_star = self.labor_supply_opt(w, p1, p2)
         l1_star = self.labor_opt(p1, w)
@@ -98,7 +101,7 @@ class ProblemOne:
         c1 = self.consumption1(w, l_star, pi1_star, pi2_star, p1, p2)
         return y1_star - c1
     
-    def good1_clear_con2(self, p1, p2, w):
+    def good1_clear_con2(self, p1, p2, w): #setting up the clearing condition for the good 1 market (ready for fsolve of p1)
         par = self.par
         l_star = self.labor_supply_opt(w, p1, p2)
         l1_star = self.labor_opt(p1, w)
@@ -110,7 +113,7 @@ class ProblemOne:
         c1 = self.consumption1(w, l_star, pi1_star, pi2_star, p1, p2)
         return y1_star - c1
 
-    def good2_clear_con(self, p2, p1, w):
+    def good2_clear_con(self, p2, p1, w): #setting up the clearing condition for the good 2 market (ready for fsolve of p2)
         par = self.par
         l_star = self.labor_supply_opt(w, p1, p2)
         l1_star = self.labor_opt(p1, w)
@@ -122,7 +125,7 @@ class ProblemOne:
         c2 = self.consumption1(w, l_star, pi1_star, pi2_star, p1, p2)
         return y2_star - c2
     
-    def good2_clear_con2(self, p1, p2, w):
+    def good2_clear_con2(self, p1, p2, w): #setting up the clearing condition for the good 2 market (ready for fsolve of p1)
         par = self.par
         l_star = self.labor_supply_opt(w, p1, p2)
         l1_star = self.labor_opt(p1, w)
